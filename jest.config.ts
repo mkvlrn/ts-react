b6b4ebab182c77bf3271ba33f7e30c43c19f7fbd
Mike Valeriano <mkvlrn@gmail.com>
2022-10-29 22:02:18 -0300

    chore: init project

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  passWithNoTests: true,
+  preset: 'ts-jest',
+  rootDir: './',
+  testRegex: '.(spec|test).(ts|tsx)$',
+  testEnvironment: 'node', // "jsdom" for react
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  transform: {
+    '^.+.(png|svg|jpg|gif|webp)$': 'jest-transform-stub',
+  },
+};
+
+export default config;
