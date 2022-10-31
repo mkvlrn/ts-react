c2bc588bbe6990293c3f9a3634401c9ce9f92a78
Mike Valeriano <mkvlrn@gmail.com>
2022-10-31 04:34:14 -0300

    chore: update base files based on guide

--- jest.config.ts
+++ jest.config.ts
@@ -7,14 +7,16 @@ const config: Config = {
   passWithNoTests: true,
   preset: 'ts-jest',
   rootDir: './',
-  testRegex: '.(spec|test).(ts|tsx)$',
-  testEnvironment: 'jsdom',
+  testRegex: '.spec.(ts|tsx)$', // "spec" for unit tests, "test" for integration or e2e
+  testEnvironment: 'node', // "jsdom" for react
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>',
   }),
   transform: {
     '^.+.(png|svg|jpg|gif|webp)$': 'jest-transform-stub',
   },
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['./src/**/*.{ts,tsx}', '!**/index.{ts,tsx}'],
 };
 
 export default config;
