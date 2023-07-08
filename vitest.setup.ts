82e28f0a60fdbe0a2ea708c929ae76c23ef3b78c
Mike Valeriano <mkvlrn@gmail.com>
2023-07-08 05:24:04 -0300

    chore: remove ts-expect-error

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,7 +2,6 @@ import matchers from '@testing-library/jest-dom/matchers.js';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect } from 'vitest';
 
-// @ts-expect-error, default import or w/e
 expect.extend(matchers);
 
 afterEach(() => {
