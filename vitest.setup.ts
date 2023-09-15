b146caac8716eedbf6f582ec658ef635febc7643
Mike Valeriano <mkvlrn@gmail.com>
2023-09-15 07:21:38 -0300

    chore: bump deps

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,8 @@
-import matchers from '@testing-library/jest-dom/matchers.js';
+import * as matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect } from 'vitest';
 
+// @ts-expect-error, jest-dom types are not exported as default
 expect.extend(matchers);
 
 afterEach(() => {
