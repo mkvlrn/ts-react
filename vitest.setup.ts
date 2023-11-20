91f32a9cc64aa337394b718140d5bc53f49ba36f
Mike Valeriano <mkvlrn@gmail.com>
2023-11-20 10:23:22 -0300

    chore: bump deps, reorganize as non-module

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,7 +2,6 @@ import * as matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect } from 'vitest';
 
-// @ts-expect-error, jest-dom types are not exported as default
 expect.extend(matchers);
 
 afterEach(() => {
